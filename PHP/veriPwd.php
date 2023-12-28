8166714eed28f25dc3cc6a012dae3d441440ea8a
Sadeesha Perera <121984253+SadeeshaPerera@users.noreply.github.com>
2023-12-29 02:07:02 +0530

    Added PHP Code Files of Normal Pages

---
+++ PHP/veriPwd.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Verify Password | LifeCare</title>
+    <link rel="stylesheet" type="text/css" href="../CSS/veriPwd.css">
+    <link rel="icon" type="image/x-icon" href="../Images/logo.png">
+</head>
+
+<body>
+    <h4><a href="./forgotPwd.php">Forgot Password</a></h4>
+    <div class="content">
+        <h1>!!Verify!!</h1>
+        <div class="verify">
+            <p>A verification code has been dispatched to your email. Please 
+            check.<br>
+            Once you receive the code, enter it in the textbox given below:<br>
+            (ex*********@gmail.com)
+            <br><br>
+                <p2>
+                    Verification Code:
+                </p2>
+            </p>
+        
+            <div class="number-input-container">
+                <input type="number" maxlength="1" class="number-input">
+                <input type="number" maxlength="1" class="number-input">
+                <input type="number" maxlength="1" class="number-input">
+                <input type="number" maxlength="1" class="number-input">
+                <input type="number" maxlength="1" class="number-input">
+            </div>
+            <div class="button">
+                <a href="./resetPwd.php"><input type="submit" value="Continue"></a>
+            </div>
+        </div>
+    </div>  
+</body>
+</html>
