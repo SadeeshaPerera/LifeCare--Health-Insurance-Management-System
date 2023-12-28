4c120d11da18b11e16f53b3bd3243a73414c7008
Sadeesha Perera <121984253+SadeeshaPerera@users.noreply.github.com>
2023-12-29 01:20:29 +0530

    Added PHP Code Files of Customer Pages

---
+++ PHP/Terms and Conditions.php
@@ -0,0 +1,59 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Terms and Conditions | LifeCare</title>
+    <link rel="stylesheet" href="../CSS/Terms and Conditions.css">
+    <link rel="icon" type="image/x-icon" href="../Images/logo.png">
+</head>
+<body>
+    <header>
+        <h1>Terms and Conditions</h1>
+    </header>
+
+    <main>
+        <div>
+            <h2>1. Intellectual Property Rights</h2>
+            <p>All content on the Website, including text, graphics, logos, images, videos, and software, is the property of Life Care Insurance or its content suppliers and is protected by intellectual property laws. You may not modify, reproduce, distribute, or create derivative works from any content without the prior written consent of Life Care Insurance.</p>
+        </div>
+
+        <div>
+            <h2>2. User Conduct</h2>
+            <p>a. You agree to use the Website only for lawful purposes and in a manner that does not infringe upon the rights of others or restrict their use and enjoyment of the Website.</p>
+            <p>b. You are solely responsible for any content you post or transmit on the Website. You agree not to engage in any activities that may:</p>
+            <ul>
+                <li>Violate any applicable laws or regulations</li>
+                <li>Infringe upon the rights of others</li>
+                <li>Invade the privacy of others</li>
+                <li>Transmit viruses or other harmful code</li>
+                <li>Interfere with the operation of the Website</li>
+            </ul>
+        </div>
+
+        <div>
+            <h2>3. Disclaimer of Warranties</h2>
+            <p>The Website is provided on an "as is" and "as available" basis. Life Care Insurance makes no warranties, expressed or implied, regarding the Website's availability, accuracy, reliability, or content. To the fullest extent permitted by law, Life Care Insurance disclaims all warranties, including but not limited to warranties of merchantability, fitness for a particular purpose, and non-infringement.</p>
+        </div>
+
+        <div>
+            <h2>4. Limitation of Liability</h2>
+            <p>In no event shall Life Care Insurance be liable for any direct, indirect, incidental, special, consequential, or exemplary damages arising out of or in connection with your use or inability to use the Website, even if Life Care Insurance has been advised of the possibility of such damages.</p>
+        </div>
+
+        <div>
+            <h2>5. Links to Third-Party Websites</h2>
+            <p>The Website may contain links to third-party websites that are not owned or controlled by Life Care Insurance. Life Care Insurance has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third-party websites. By using the Website, you expressly release Life Care Insurance from any and all liability arising from your use of any third-party website.</p>
+        </div>
+
+        <div>
+            <h2>6. Changes to the Agreement</h2>
+            <p>Life Care Insurance reserves the right to modify or replace this Agreement at any time without prior notice. It is your responsibility to review this Agreement periodically for any changes. Your continued use of the Website after the posting of any changes constitutes acceptance of those changes.</p>
+        </div>
+
+        <div>
+            <h2>7. Governing Law and Jurisdiction</h2>
+            <p>This Agreement shall be governed by and construed in accordance with the laws of Sri Lanka. Any disputes arising under or in connection with this Agreement shall be subject to the exclusive jurisdiction of the courts located in Sri Lanka.</p>
+        </div>
+    </main>
+</body>
+</html>
