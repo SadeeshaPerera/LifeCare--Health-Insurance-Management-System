bf6267bf70f58cbe4a5f144d896a75a053710db7
Sadeesha Perera <121984253+SadeeshaPerera@users.noreply.github.com>
2023-12-29 01:23:51 +0530

    Added Admin Page CRUD Code Files

---
+++ Admin Pages/CRUD for Manage Policy/create.php
@@ -0,0 +1,214 @@
+<?php include "header_After_login1.php"?>
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "iwt_assignment";
+
+//create connection
+
+$connection = new mysqli($servername, $username,$password,$database,3307);
+
+
+ $Policy_ID="";
+ $Pkg_ID="";
+ $Policy_Type="";
+ $Effective_Date="";
+ $Expiration_Date="";
+ $Policy_Status="";
+
+
+ $errorMessage = "";
+ $successMessage = "";
+
+ if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $Policy_ID=$_POST["Policy_ID"];
+    $Pkg_ID=$_POST["Pkg_ID"];
+    $Policy_Type=$_POST["Policy_Type"];
+    $Effective_Date=$_POST["Effective_Date"];
+    $Expiration_Date=$_POST["Expiration_Date"];
+    $Policy_Status=$_POST["Policy_Status"];
+
+   
+      do{
+           if(empty($Policy_ID) || empty($Pkg_ID) || empty($Policy_Type) ||  empty($Effective_Date) ||  empty($Effective_Date) ||  empty($Policy_Status) ){
+             
+            $errorMessage = "All the fields are required";
+            break;
+
+           }
+
+           //add new client to database
+            
+           $sql = "INSERT INTO Policy(Policy_ID,Pkg_ID,Policy_Type,Effective_Date,Expiration_Date,Policy_Status)" .
+                  "VALUES ('$Policy_ID','$Pkg_ID','$Policy_Type','$Effective_Date','$Expiration_Date','$Policy_Status')";
+                  $result= $connection->query($sql);
+
+                  if(!$result){
+                    $errorMessage = "Invalid Input:" . $connection->error;
+                    break;
+                  }
+
+           $Policy_ID="";
+           $Pkg_ID="";
+           $Policy_Type="";
+           $Effective_Date="";
+           $Expiration_Date="";
+           $Policy_Status="";
+          
+
+           $successMessage = "Added correctly";
+      
+           header("location: index.php");
+           exit;
+
+
+      }while(false);
+ }
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="./Images/logo.png">
+    <title>Add New Policy</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+
+    <style>
+        #policy-body{
+            margin-top:150px;
+        }
+    </style>
+</head>
+<body id ="policy-body">
+
+     <div class="container my-5">
+        <h2 style="margin-bottom:25px;">New Policy</h2>
+
+
+
+        <?php 
+            if( !empty($errorMessage)){
+                echo "
+                <div class='alert alert-warning alert-dismissible fade show' role='alert'>
+                    <strong>$errorMessage</strong>
+                    <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                </div>
+            ";
+            
+
+            }
+
+
+           
+
+
+        ?>
+       
+
+
+
+            
+        <form method="post">
+
+            <div class="row mb-3" >
+                <label  class="col-sm-3">Policy_ID</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="Policy_ID" value="<?php echo $Policy_ID?>">
+                </div>
+            </div>
+
+            <div class="row mb-3" >
+                <label  class="col-sm-3">Pkg_ID</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="Pkg_ID" value="<?php echo $Pkg_ID?>">
+                </div>
+            </div>
+
+
+            <div class="row mb-3" >
+                <label  class="col-sm-3">Policy_Type</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="Policy_Type" value="<?php echo $Policy_Type?>">
+                </div>
+            </div>
+
+            <div class="row mb-3" >
+                <label  class="col-sm-3">Effective_Date</label>
+                <div class="col-sm-6">
+                    <input type="date" class="form-control" name="Effective_Date" value="<?php echo $Effective_Date?>">
+                </div>
+            </div>
+
+
+            <div class="row mb-3" >
+                <label  class="col-sm-3">Expiration_Date</label>
+                <div class="col-sm-6">
+                    <input type="date" class="form-control" name="Expiration_Date" value="<?php echo $Expiration_Date?>">
+                </div>
+            </div>
+
+
+            <div class="row mb-3" >
+                <label  class="col-sm-3">Policy_Status</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="Policy_Status" value="<?php echo $Policy_Status?>">
+                </div>
+            </div>
+
+            <?php   
+             
+             if(!empty($successMessage))
+             echo "
+             <div class='row mb-3'>
+                 <div class='offset-sm-3 col-sm-6'>
+                     <div class='alert alert-success alert-dismissible fade show' role='alert'>
+                         <strong>$successMessage</strong>
+                         <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                     </div>
+                 </div>
+             </div>
+         ";
+             
+             
+             
+             
+             ?>
+
+
+
+
+
+
+
+
+
+            <div class="row mb-3">
+                <div class="offset-sm-3 col-sm-3 d-grid">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div> 
+
+                
+
+
+            </div>
+
+
+
+
+
+
+
+
+        </form>
+
+    </div>
+    
+</body>
+</html>
---
+++ Admin Pages/CRUD for Manage Policy/index.php
@@ -0,0 +1,90 @@
+<?php include "header_After_login1.php"?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="./Images/logo.png">
+    <title>Manage Policy | LifeCare</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <style>
+        #policy-body{
+            margin-top:150px;
+        }
+    </style>
+</head>
+<body id ="policy-body">
+    <div class="container my-5">
+         <h2>Policy Details</h2><br>
+        
+         <a class="btn btn-primary" href="create.php" role="button">ADD NEW</a>
+         <br><br>
+
+
+         <table class="table">
+            <thead>
+                <tr>
+                    <th>Policy_ID</th>
+                    <th>Pkg_ID</th>
+                    <th>Policy_Type</th>
+                    <th>Effective_Date</th>
+                    <th>Expiration_Date</th>
+                    <th>Policy_Status</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+
+            <tbody>
+
+            <?php
+              $servername = "localhost";
+              $username = "root";
+              $password = "";
+              $database = "iwt_assignment";
+
+              //create connection
+
+              $connection = new mysqli($servername, $username,$password,$database,3307);
+              
+                     if($connection -> connect_error){
+                        die("connection failed:" . $connection->connect_error);
+                     }
+
+                     //read all row from table
+                    $sql = "SELECT * FROM Policy";
+                    $result = $connection->query($sql);
+
+                    if(!$result){
+                        die("Invalid query:" . $connection_error);
+                    }
+
+
+                    while($row = $result->fetch_assoc()){
+                        echo "
+                        <tr>
+                    <td>$row[Policy_ID]</td>
+                    <td>$row[Pkg_ID]</td>
+                    <td>$row[Policy_Type]</td>
+                    <td>$row[Effective_Date]</td>
+                    <td>$row[Expiration_Date]</td>
+                    <td>$row[Policy_Status]</td>
+
+                    <td>
+                        <a class='btn btn-primary btn-sm' href='edit.php?Policy_ID=$row[Policy_ID]'>Edit</a>
+                        <a class='btn btn-danger btn-sm' href='delete.php?Policy_ID=$row[Policy_ID]'>Delete</a>
+                    </td>
+                </tr>
+                        ";
+
+                    }
+                       
+            ?>    
+
+                
+            </tbody>
+         </table>
+         
+    </div>
+</body>
+</html>
